94194a64a1027da2f2b88d202f1f1c7df81c9051
CammySummons <64822645+CammySummons@users.noreply.github.com>
2022-02-06 21:10:23 +1300

    Update Ex_8_area_and_volume_of_sphere.py

---
+++ Ex_8_area_and_volume_of_sphere.py
@@ -0,0 +1,5 @@
+radius = float(input("Radius of sphere (cm): "))
+volume = 4/3 * 3.14159 * (radius**3)
+area = 4 * 3.14159 * (radius**2)
+
+print("Volume: {} cm^3\nArea: {} cm^2".format(volume, area))
