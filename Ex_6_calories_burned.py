5357e2b7066ba9fce09f170d5d74756970b35f06
CammySummons <64822645+CammySummons@users.noreply.github.com>
2022-02-06 20:59:34 +1300

    Updates

---
+++ Ex_6_calories_burned.py
@@ -0,0 +1,10 @@
+
+bicycling = int(input("Number of hours spent bicycling: "))
+jogging = int(input("Number of hours spent jogging: "))
+swimming = int(input("Number of hours spent swimming: "))
+
+calories = bicycling*200 + jogging*475 + swimming*275
+kilograms = (calories/(3500/454))/1000
+
+print(calories)
+print(kilograms)
