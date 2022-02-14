809c5409e10facacf28c6525b082c31db7aab611
CammySummons <64822645+CammySummons@users.noreply.github.com>
2022-02-14 17:47:50 +1300

    Update Times_v2.py

--- Times_v2.py
+++ Times_v2.py
@@ -1,10 +1,29 @@
-track_type = input("What track type? (100m, 200m, 400m): ")
+times_100m = []
+times_200m = []
+times_400m = []
+valid = False
+track_type = ""
+go = "Y"  # Decides whether to loop grab_times or not
 
 
-def grab_times():
+def print_results(tracks):
+    total = 0
+    fastest = 10000000
+    for result in tracks:
+        print(f"- {result}s")
+        total += int(result)
+        if fastest > result:
+            fastest = result
+
+    average = total/len(tracks)
+
+    print(f"Fastest time: {fastest}s")
+    print(f"Average time: {average}s\n")
+
+
+def grab_times(times):
     time = ""
-    times = []
-    fastest = 0
+    fastest = 100000
     while time != -1:
         time = int(input("Enter a time (or type -1 to finish): "))
         if time != -1:
@@ -15,14 +34,54 @@ def grab_times():
     for item in times:
         print(item)
         total += int(item)
-        if fastest < item:
+        if fastest > item:
             fastest = item
 
     average = total/len(times)
 
     print(f"Fastest time: {fastest}")
     print(f"Average time: {average}")
+    proceed = input("Do you want to enter more times? (Y/N): ").upper()
+    return proceed
 
 
-if track_type == "100m":
-    grab_times()
+# Getting track type and running grab_times function
+while go != "N":
+    track_type = input("What track type? (100m, 200m, 400m): ")
+    if track_type == "100m":
+        go = grab_times(times_100m)
+
+    elif track_type == "200m":
+        go = grab_times(times_200m)
+
+    elif track_type == "400m":
+        go = grab_times(times_400m)
+
+# Decides if you want to clear data
+while not valid:
+    clear = input("Enter track type for data you want to clear (else enter -1): ")
+    if clear == "100m":
+        times_100m = []
+        print("Times cleared")
+    elif clear == "200m":
+        times_200m = []
+        print("Times cleared")
+    elif clear == "400m":
+        times_400m = []
+        print("Times cleared")
+    elif clear == "-1":
+        valid = True
+    else:
+        valid = False
+
+# Printing results
+print("\n----All Results----\n")
+if times_100m != []:
+    print("**** 100m Track Times ****")
+    print_results(times_100m)
+if times_200m != []:
+    print("**** 200m Track Times ****")
+    print_results(times_200m)
+if times_400m != []:
+    print("**** 400m Track Times ****")
+    print_results(times_400m)
